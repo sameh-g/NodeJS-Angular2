7c9a9d78c71728e431d4592e18752340b6f30d8f
Sameh George <sameh.george@itworx.com>
2016-10-31 16:57:48 +0200

    init commit

---
+++ server/routes/public.ts
@@ -0,0 +1,12 @@
+import { Router, Response, Request } from 'express';
+
+const publicRouter: Router = Router();
+
+publicRouter.get('/simple', (request: Request, response: Response) => {
+  response.json({
+    text: 'Greetings.',
+    title: 'Public routes'
+  });
+});
+
+export { publicRouter }
